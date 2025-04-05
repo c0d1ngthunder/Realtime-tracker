e53f574aea6dc86e9a957dce2ef246e7f633733e
Nitish Kumar <codingthunder3@gmail.com>
2025-04-05 21:57:39 +0530

    feat: center map on user location using geolocation

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+});
+
 app.get("/", (req, res) => {
   res.render("index");
 });
