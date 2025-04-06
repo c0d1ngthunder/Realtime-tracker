9c285125a3b632bac292931e9edae6e58d3d86b9
Nitish Kumar <codingthunder3@gmail.com>
2025-04-06 09:31:04 +0530

    fix: fix location accuracy issues

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,15 +3,19 @@ const socket = io();
 if (navigator.geolocation) {
   navigator.geolocation.watchPosition(
     (position) => {
-      const { latitude, longitude } = position.coords;
-      socket.emit("send-location", { latitude, longitude });
+      const { latitude, longitude, accuracy } = position.coords;
+      if (accuracy < 1000) {
+        socket.emit("send-location", { latitude, longitude });
+      } else {
+        console.error("Accuracy is too low:", accuracy);
+      }
     },
     (err) => {
       console.error(err);
     },
     {
       enableHighAccuracy: true,
-      timeout: 5000,
+      timeout: 10000,
       maximumAge: 0,
     }
   );
@@ -27,9 +31,9 @@ const markers = {};
 socket.on("receive-location", (data) => {
   const { id, latitude, longitude } = data;
   map.setView([latitude, longitude]);
-  if (markers[id]){
-    markers[id].setLatLng([latitude,longitude]);
-  }else{
-    markers[id] = L.marker([latitude,longitude]).addTo(map);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
   }
 });
